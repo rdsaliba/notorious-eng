fa2332fda806f250572379bda6157e147c164421
Najim Ghafourzadeh <najimg1@hotmail.com>
2020-11-02 02:18:54 -0500

    Added H2dependency and h2.jar file in project classpath. Created a sample in-memory database with basic sql queries.

--- src/main/java/com/cbms/app/Main.java
+++ src/main/java/com/cbms/app/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main   {
 
    /* @Override
@@ -20,9 +22,36 @@ public class Main   {
 
     public static void main(String[] args) throws Exception {
         
-        StartupController startupController = new StartupController();
-        startupController.generateModel();
+    //    StartupController startupController = new StartupController();
+    //    startupController.generateModel();
         
   //  launch(args);
+
+        String url = "jdbc:h2:mem:cbms_db";
+        String user = "sa";
+        String password = "";
+        Connection conn = null;
+        try {
+            Class.forName("org.h2.Driver");
+            conn = DriverManager.getConnection(url,user,password);
+            System.out.println("Connection Created");
+
+            Statement stmt = conn.createStatement();
+
+            stmt.execute("CREATE TABLE test(id INT PRIMARY KEY, name VARCHAR(20))");
+            System.out.println("Table Created");
+
+            int insertedrows = stmt.executeUpdate("INSERT INTO test VALUES(1, 'one')");
+            System.out.println("Rows inserted: " + insertedrows);
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM test");
+
+            while(rs.next())
+                System.out.println(rs.getString("name"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
