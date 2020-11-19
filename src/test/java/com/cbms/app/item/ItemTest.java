2e0c6f2e7740b291bff35519e9cc2b47fb3e0e0b
PaulMicu <micu.p@outlook.com>
2020-11-19 11:40:59 -0500

    Unit tests for Item Class
    Class coverage : 100%
    Method Coverage: 100%
    Line Coverage: 100%

---
+++ src/test/java/com/cbms/app/item/ItemTest.java
@@ -0,0 +1,45 @@
+package com.cbms.app.item;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ItemTest {
+    private static Item item;
+
+    @BeforeClass
+    public static void setup(){
+        item = new Item(123,"usertag");
+    }
+
+    @Test
+    public void defaultCons(){
+        Item temp = new Item();
+        assertNotNull(temp);
+    }
+
+    @Test
+    public void getId() {
+        assertEquals("Current ID should be 123", 123, item.getId());
+    }
+
+    @Test
+    public void setId() {
+        item.setId(555);
+        assertEquals("ID after change should be 555", 555, item.getId());
+
+    }
+
+    @Test
+    public void getUserTag() {
+        assertEquals("Current usertag should be 'usertag'", "usertag", item.getUserTag());
+    }
+
+    @Test
+    public void setUserTag() {
+        item.setUserTag("newUserTag");
+        assertEquals("Usertag after change should be 'newUserTag'","newUserTag",item.getUserTag());
+    }
+}
