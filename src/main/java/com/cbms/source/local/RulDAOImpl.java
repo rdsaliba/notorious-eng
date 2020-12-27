10c30e4ebbe4945c929845dda5a76c1d7b9c1aa4
PaulMicu <micu.p@outlook.com>
2020-12-27 12:39:29 -0500

    added some comments for the new functions of the RulDAO and the RulDAOImpl objects

--- src/main/java/com/cbms/source/local/RulDAOImpl.java
+++ src/main/java/com/cbms/source/local/RulDAOImpl.java
@@ -1,3 +1,11 @@
+/*
+  Implementation of the DAO design pattern
+  This class extends the general DAO object and implements the RulDAO interface
+
+  @author      Paul Micu
+  @version     1.0
+  @last_edit   12/27/2020
+ */
 package com.cbms.source.local;
 
 import java.sql.PreparedStatement;
@@ -7,6 +15,13 @@ import java.sql.SQLException;
 public class RulDAOImpl extends DAO implements RulDAO {
     public static final String GET_LATEST_RUL_FROM_ASSET_ID = "SELECT value FROM asset_model_calculation where asset_id = ? ORDER BY `timestamp` desc LIMIT 1";
 
+    /**
+     * Given an asset id this function will return the double value of
+     * the latest RUL measurement regardless of the model or timestamp
+     *
+     * @param assetID represents an asset's id
+     * @author Paul
+     */
     @Override
     public double getLatestRUL(int assetID) {
         double estimate = -100000;
