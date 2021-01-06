bd69b08052cdd1bb71fc66bcece6ce185b86451b
JayChouteau <47158909+JayChouteau@users.noreply.github.com>
2021-01-06 14:09:56 -0500

    Add system UI refactoring (#134) - Text field larger for tablet users - Dropdown for system type (only UI) - Text wrapping for the text area

--- src/main/java/com/cbms/ui/controller/AddSystemController.java
+++ src/main/java/com/cbms/ui/controller/AddSystemController.java
@@ -8,20 +8,33 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
+
+import java.sql.ResultSet;
 
 public class AddSystemController implements Initializable {
 
     @FXML
-    private Button systemMenuButton;
+    public Button systemMenuBtn;
     @FXML
-    private Button cancelButton;
+    private Button cancelBtn;
+    @FXML
+    private Button saveBtn;
+    @FXML
+    private TextArea systemDescriptionTextArea;
+    @FXML
+    private ChoiceBox systemTypeChoiceBox;
 
+    private static final String GET_ASSET_TYPES = "SELECT name FROM asset_type";
     private UIUtilities uiUtilities;
 
     /**
@@ -37,6 +50,8 @@ public class AddSystemController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         uiUtilities = new UIUtilities();
         attachEvents();
+        initializeFieldValues();
+        systemDescriptionTextArea.setWrapText(true);
     }
 
     /**
@@ -46,18 +61,39 @@ public class AddSystemController implements Initializable {
      */
     public void attachEvents() {
         // Change scenes to Systems.fxml
-        systemMenuButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                uiUtilities.changeScene(mouseEvent, "/Systems");
-            }
-        });
+        systemMenuBtn.setOnMouseClicked(mouseEvent -> uiUtilities.changeScene(mouseEvent, "/Systems"));
         // Change scenes to Systems.fxml
-        cancelButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                uiUtilities.changeScene(mouseEvent, "/Systems");
+        cancelBtn.setOnMouseClicked(mouseEvent -> uiUtilities.changeScene(mouseEvent, "/Systems"));
+    }
+
+    /**
+     * Initializes the default and possible values for all fields that can accept user input. For example,
+     * it establishes the possible dropdown values for the system type selection.
+     */
+    public void initializeFieldValues(Connection con) {
+        try (Statement stmt = con.createStatement();) {
+            ResultSet rs = stmt.executeQuery(GET_ASSET_TYPES);
+            while (rs.next()) {
+                systemTypeChoiceBox.getItems().add(rs);
             }
-        });
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void executeStatement(Connection con) {
+        try(Statement stmt = con.createStatement();) {
+            String SQL = "SELECT LastName, FirstName FROM Person.Contact ORDER BY LastName";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("LastName") + ", " + rs.getString("FirstName"));
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     }
 }
