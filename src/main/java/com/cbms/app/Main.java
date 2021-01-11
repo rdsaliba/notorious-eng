ee593ac3dd3923ca8016897e7457826b7d559d20
PaulMicu <micu.p@outlook.com>
2021-01-10 22:33:34 -0500

    added the option to turn on real time mode and that will simulate new data coming in every second until all data is read

--- src/main/java/com/cbms/app/Main.java
+++ src/main/java/com/cbms/app/Main.java
@@ -1,19 +1,38 @@
 package com.cbms.app;
 
 
+import com.cbms.source.local.AssetDAOImpl;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Main extends Application {
-
+    private static int time=1;
+    private static boolean realTime=true;
     public static void main(String[] args) {
+
         ModelController modelController = ModelController.getInstance();
+
+        if (realTime){
+            AssetDAOImpl assetDAO = new AssetDAOImpl();
+            assetDAO.resetAssetForLive();
+            System.out.println("timer start");
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    time++;
+                    System.out.println(time);
+                }
+            }, 0, 1000);
+        }
         modelController.initializer();
         launch(args);
     }
@@ -23,6 +42,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/Systems.fxml"));
         Scene sample = new Scene(root);
         primaryStage.setTitle("CBMS");
+       // primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(sample);
         primaryStage.show();
         primaryStage.setOnCloseRequest(e -> {
@@ -38,5 +58,13 @@ public class Main extends Application {
         System.out.println("Program closing.");
         Platform.exit();
         System.exit(0);
+    }
+
+    public static boolean isRealTime(){
+        return realTime;
+    }
+
+    public static int getTime(){
+        return time;
     }
 }
