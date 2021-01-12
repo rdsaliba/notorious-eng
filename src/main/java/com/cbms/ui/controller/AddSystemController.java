d2c51553f42fe8382321cfda80ac8ace8abbc75c
Jeff Buonamici <jeffbuonamici@users.noreply.github.com>
2021-01-12 01:40:57 -0500

    #11 Assemble data from UI and create Asset object.

--- src/main/java/com/cbms/ui/controller/AddSystemController.java
+++ src/main/java/com/cbms/ui/controller/AddSystemController.java
@@ -1,13 +1,20 @@
 package com.cbms.ui.controller;
 
+import com.cbms.app.item.Asset;
+import com.cbms.app.item.AssetType;
+import com.cbms.source.local.AssetDAOImpl;
 import com.cbms.source.local.AssetTypeDAOImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.*;
@@ -21,13 +28,30 @@ public class AddSystemController implements Initializable {
     @FXML
     private Button saveBtn;
     @FXML
+    private ChoiceBox<AssetType> systemTypeChoiceBox;
+    @FXML
+    private TextField systemNameInput;
+    @FXML
     private TextArea systemDescriptionTextArea;
     @FXML
-    private ChoiceBox<String> systemTypeChoiceBox;
+    private TextField serialNumberInput;
+    @FXML
+    private TextField manufacturerInput;
+    @FXML
+    private TextField categoryInput;
+    @FXML
+    private TextField siteInput;
+    @FXML
+    private TextField locationInput;
 
-    private static ObservableList<String> assetTypeNamesList;
+
+
+
+    private static ObservableList<AssetType> assetTypeNamesList;
+    private AssetDAOImpl assetDAOImpl;
     private AssetTypeDAOImpl assetTypeDAOImpl;
     private UIUtilities uiUtilities;
+    private AssetType selectedAssetType;
 
     /**
      * Initialize runs before the scene is displayed.
@@ -40,6 +64,7 @@ public class AddSystemController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        assetDAOImpl = new AssetDAOImpl();
         assetTypeDAOImpl = new AssetTypeDAOImpl();
         uiUtilities = new UIUtilities();
         attachEvents();
@@ -53,6 +78,16 @@ public class AddSystemController implements Initializable {
      * @author Jeff
      */
     public void attachEvents() {
+        systemTypeChoiceBox.valueProperty().addListener((obs, oldval, newval) -> {
+            if(newval != null)
+                selectedAssetType = newval;
+        });
+        saveBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                saveAsset(assembleAsset());
+            }
+        });
         // Change scenes to Systems.fxml
         systemMenuBtn.setOnMouseClicked(mouseEvent -> uiUtilities.changeScene(mouseEvent, "/Systems"));
         // Change scenes to Systems.fxml
@@ -67,5 +102,49 @@ public class AddSystemController implements Initializable {
         // Establishes the asset types available for selection in the dropdown
         assetTypeNamesList = FXCollections.observableArrayList(assetTypeDAOImpl.getAssetTypeList());
         systemTypeChoiceBox.setItems(assetTypeNamesList);
+        systemTypeChoiceBox.setValue(systemTypeChoiceBox.getItems().get(0));
+        systemTypeChoiceBox.setConverter(new StringConverter<AssetType>() {
+            @Override
+            public String toString(AssetType assetType) {
+                return assetType.getName();
+            }
+
+            @Override
+            public AssetType fromString(String s) {
+                return systemTypeChoiceBox.getItems().stream().filter(ap ->
+                        ap.getName().equals(s)).findFirst().orElse(null);
+            }
+        });
+    }
+
+    public Asset assembleAsset() {
+        Asset newAsset = new Asset();
+        newAsset.setName(systemNameInput.getText());
+        newAsset.setAssetTypeID(selectedAssetType.getId());
+        newAsset.setDescription(systemDescriptionTextArea.getText());
+        newAsset.setSerialNo(serialNumberInput.getText());
+        newAsset.setManufacturer(manufacturerInput.getText());
+        newAsset.setCategory(categoryInput.getText());
+        newAsset.setSite(siteInput.getText());
+        newAsset.setLocation(locationInput.getText());
+        return newAsset;
+    }
+
+    /**
+     * Creates an ObservableList of asset type names.
+     *
+     * @param assetTypeList
+     * @return
+     */
+    public ObservableList<String> getAssetTypeNameList(ObservableList<AssetType> assetTypeList) {
+        ObservableList<String> assetTypeNames = FXCollections.observableArrayList();
+        for (AssetType assetType:assetTypeList) {
+            assetTypeNames.add(assetType.getName());
         }
+        return assetTypeNames;
+    }
+
+    public void saveAsset(Asset newAsset) {
+        //call assetDAOImpl method.
+    }
 }
