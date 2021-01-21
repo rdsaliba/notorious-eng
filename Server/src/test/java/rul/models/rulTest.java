4c25d5599c72ce94820e8eccae5897fa858a188a
Talal <bazerbachi.talal@gmail.com>
2021-01-21 23:11:58 +0200

    194 Fix errors in RUL unit tests

--- Server/src/test/java/rul/models/rulTest.java
+++ Server/src/test/java/rul/models/rulTest.java
@@ -1,15 +1,13 @@
 package rul.models;
-import com.cbms.app.item.AssetAttribute;
-import com.cbms.rul.models.LinearRegressionModelImpl;
+//import com.cbms.app.item.AssetAttribute;
+//import com.cbms.rul.models.LinearRegressionModelImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import rul_models.testModel;
 import weka.classifiers.Classifier;
 import weka.classifiers.functions.LinearRegression;
 import weka.core.Instances;
-
-import java.RUL_Models.RootMeanSquaredError;
-import java.RUL_Models.testModel;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,22 +18,17 @@ import static org.junit.Assert.*;
 
 public class rulTest {
     private LinearRegressionModelImpl lr;
-    private RootMeanSquaredError error;
-    private testModel testMod;
+
 
     @Before
     public void setUp(){
         lr = new LinearRegressionModelImpl();
-        error = new RootMeanSquaredError();
-        testMod = new testModel();
 
     }
 
     @After
     public void tearDown() {
         lr = null;
-        error = null;
-        testMod = null;
     }
 
     @Test
@@ -53,25 +46,5 @@ public class rulTest {
         LinearRegression linearRegression = new LinearRegression();
         assertEquals("Should Return Linear Regression Model", linearRegression.getClass(), lr.trainModel(set).getClass());
 
-    }
-
-    @Test
-    public void calculate() {
-        double[] first= new double[]{1.1, 1.2, 1.3};
-        Double[] second= new Double[]{2.1, 2.2, 2.3};
-        assertEquals("Function should return ",1 , error.calculate(first, second));
-    }
-
-    @Test
-    public void parseRULs() throws IOException {
-        FileReader read= new FileReader("/Users/talalbazerbachi/Documents/GitHub/notorious-eng/Dataset/Converted/FD01_Train_RUL.arff");
-        Double[] t = new Double[0];
-        assertEquals("Should return list", t.getClass(), testMod.parseRULs(read).getClass());
-    }
-
-    @Test
-    public void showPredictedRULs() {
-        double[] max = new double[0];
-        assertEquals("Should return done", "done", testMod.showPredictedRUL(max));
     }
 }
