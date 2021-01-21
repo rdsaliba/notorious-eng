7396b9b16f643a81455e663c6ae89e6e6fe79421
Roy Saliba <rsaliba95@icloud.com>
2021-01-21 11:05:32 -0500

    Fix for rows change scene mechanism

--- JavaFXUserInterface/src/main/java/UIUtilities.java
+++ JavaFXUserInterface/src/main/java/UIUtilities.java
@@ -49,16 +49,18 @@ public class UIUtilities {
     public void changeScene(MouseEvent mouseEvent, TableRow<Asset> row, String fxmlFileName, Asset asset) {
         Stage primaryStage = (Stage) row.getScene().getWindow();
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/SystemInfo.fxml"));
-            Parent systemsParent = loader.load();
-            Scene systemInfo = new Scene(systemsParent);
+            if(!row.isEmpty()){
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/SystemInfo.fxml"));
+                Parent systemsParent = loader.load();
+                Scene systemInfo = new Scene(systemsParent);
 
-            Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-            window.setScene(systemInfo);
-            SystemInfoController controller = loader.getController();
-            controller.initData(asset);
-            window.show();
+                Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+                window.setScene(systemInfo);
+                SystemInfoController controller = loader.getController();
+                controller.initData(asset);
+                window.show();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
