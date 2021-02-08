6066a85ebefc2dc0b957a86a86041cd006beb6dc
PaulMicu <micu.p@outlook.com>
2021-02-07 23:54:50 -0500

    #239 Changed mouse event click on generate thumbnail to be consistent with the rest of the app

--- JavaFXUserInterface/src/main/java/Utilities/UIUtilities.java
+++ JavaFXUserInterface/src/main/java/Utilities/UIUtilities.java
@@ -92,22 +92,10 @@ public class UIUtilities {
      */
     public void changeScene(MouseEvent mouseEvent, TableRow<Asset> row, String fxmlFileName, Asset asset) {
         row.getScene().getWindow();
-        try {
-            if (!row.isEmpty()) {
-                FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(getClass().getResource(fxmlFileName + ".fxml"));
-                Parent systemsParent = loader.load();
-                Scene systemInfo = new Scene(systemsParent);
-
-                Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-                window.setScene(systemInfo);
-                SystemInfoController controller = loader.getController();
-                controller.initData(asset);
-                window.show();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (!row.isEmpty()) {
+            changeScene(mouseEvent, fxmlFileName, asset);
         }
+
     }
 
     /**
@@ -141,4 +129,31 @@ public class UIUtilities {
         }
     }
 
+    /**
+     * Changes scenes once an element is clicked, and
+     * sends an asset to the new scene's controller.
+     *
+     * @param mouseEvent   the mouse click event
+     * @param fxmlFileName the name of the fxml file we want to navigate to
+     * @param asset        the asset we want to sent to the other page
+     * @author Paul
+     */
+    public void changeScene(MouseEvent mouseEvent, String fxmlFileName, Asset asset) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource(fxmlFileName + ".fxml"));
+            Parent systemTypeParent = loader.load();
+            Scene systemTypeInfo = new Scene(systemTypeParent);
+
+            Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            window.setScene(systemTypeInfo);
+            SystemInfoController controller = loader.getController();
+            controller.initData(asset);
+            window.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
