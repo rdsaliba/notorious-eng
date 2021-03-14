f2d076d3ed7be2f2d5c272d46da9c78eb4941587
Talal <bazerbachi.talal@gmail.com>
2021-03-15 01:22:56 +0200

    #276 removed unused objects from evaluation

--- Server/src/main/java/app/Evaluation.java
+++ Server/src/main/java/app/Evaluation.java
@@ -1,35 +1,21 @@
 package app;
 
-import app.item.Asset;
 import app.item.TrainedModel;
-import local.AssetDAO;
 import local.AssetDAOImpl;
 import local.ModelDAOImpl;
-import preprocessing.DataPrePreprocessorController;
 import rul.models.*;
 import weka.classifiers.Classifier;
-import weka.core.Attribute;
-import weka.core.DenseInstance;
 import weka.core.Instances;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Evaluation implements Runnable {
-    private AssetDAOImpl assetDaoImpl;
-    private ModelController modelController;
     private ModelDAOImpl modelDAOImpl;
     private TrainedModel model;
     private Instances trainDataset;
     private Instances testDataset;
     public Evaluation(){
-        assetDaoImpl = new AssetDAOImpl();
-        modelController = ModelController.getInstance();
         modelDAOImpl = new ModelDAOImpl();
     }
     public Evaluation(TrainedModel model, Instances trainDataset, Instances testDataset){
-        assetDaoImpl = new AssetDAOImpl();
-        modelController = ModelController.getInstance();
         modelDAOImpl = new ModelDAOImpl();
         this.model = model;
         this.trainDataset = trainDataset;
