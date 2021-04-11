d6dc8a893bd13785b2d4f17dd10f05fd6204354d
Shirwa Warsame <shirwarsame@gmail.com>
2021-04-11 09:30:20 -0400

    #270 Unit Tests - Unit tests for local folder in Server

--- Server/src/test/java/UnitTests/local/AssetDAOImplTest.java
+++ Server/src/test/java/UnitTests/local/AssetDAOImplTest.java
@@ -6,9 +6,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static local.DatabaseConnection.getConnection;
 import static org.junit.Assert.assertEquals;
 
 public class AssetDAOImplTest {
@@ -56,26 +60,55 @@ public class AssetDAOImplTest {
 
     @Test
     public void addRULEstimation() {
-
+        String addRULQuery="INSERT INTO asset_model_calculation values( 1417 , 1 ,now(), 89.2)";
+        try (PreparedStatement ps = getConnection().prepareStatement(addRULQuery)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                assertEquals(1417,rs.getInt("asset_id"));
+                }
+            } catch (SQLException e) {
+        }
     }
 
     @Test
     public void updateRecommendation() {
+        String updateAsset="UPDATE asset set recommendation = \"Ok\" WHERE asset_id = 101";
+        try (PreparedStatement ps = getConnection().prepareStatement(updateAsset)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                assertEquals("Ok",rs.getString("recommendation"));
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Test
     public void resetAssetUpdate() {
+        String resetAsset = "UPDATE asset set updated = 0 where asset_id = 69";
+        try (PreparedStatement ps = getConnection().prepareStatement(resetAsset)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                assertEquals(0,rs.getInt("updated"));
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Test
     public void setAssetUpdate() {
+        String resetAsset = "UPDATE asset set updated = 1 where asset_id = 42";
+        try (PreparedStatement ps = getConnection().prepareStatement(resetAsset)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                assertEquals(1,rs.getInt("updated"));
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Test
     public void createAssetFromQueryResult() {
+
     }
 
     @Test
     public void createAssetInfo() {
+
     }
 }
--- Server/src/test/java/UnitTests/local/ModelDAOImplTest.java
+++ Server/src/test/java/UnitTests/local/ModelDAOImplTest.java
@@ -6,9 +6,14 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import static local.DatabaseConnection.getConnection;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ModelDAOImplTest {
     private ModelDAOImpl modelDAO;
@@ -24,10 +29,12 @@ public class ModelDAOImplTest {
 
     @Test
     public void convertToByteString() {
+
     }
 
     @Test
     public void convertFromByteString() {
+
     }
 
     @Test
@@ -44,19 +51,36 @@ public class ModelDAOImplTest {
 
     @Test
     public void setModelToTrain() {
+        String updateObject ="UPDATE trained_model tm, model m \n" +
+                                "SET tm.retrain = false, tm.serialized_model = 100 \n" +
+                                "WHERE tm.model_id = 1 AND tm.asset_type_id = 1 AND tm.status_id = 1 AND tm.model_id = m.model_id AND m.archived = 0";
+        try (PreparedStatement ps = getConnection().prepareStatement(updateObject)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                assertNotNull(rs.getBlob("serialized_object"));
+            }
+        } catch (SQLException e) {
+        }
     }
 
     @Test
     public void getModelsByAssetTypeID() {
-        //Continue with
-        TrainedModel tm = modelDAO.getModelsByAssetTypeID("1",2);
+        TrainedModel tm = modelDAO.getModelsByAssetTypeID("3",2);
+        assertEquals(3,tm.getAssetTypeID());
     }
 
     @Test
     public void createTrainedModelFromResultSet() {
+
     }
 
     @Test
     public void updateEvaluationRMSE() {
+        String updateRMSE = "UPDATE trained_model SET rmse = 25, retrain = 0 WHERE model_id = 1 AND asset_type_id = 1 AND status_id=2";
+        try (PreparedStatement ps = getConnection().prepareStatement(updateRMSE)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                assertEquals(25,rs.getDouble("rmse"),0.01);
+            }
+        } catch (SQLException e) {
+        }
     }
 }
